cb0e6ede7e8b5de17ea38143ce7886ce0b83f3c0
honorezemagho <honorezemagho@gmail.com>
2020-05-27 20:49:09 +0200

    adding liked properky in our fake movie service and adding like component in MoviesComponent

--- src/components/MoviesComponent.jsx
+++ src/components/MoviesComponent.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {getMovies} from "../services/fakeMovieService";
+import Like from './common/like';
+import Pagination from "./common/pagination";
 
 class Movies extends Component{
 state = {
@@ -8,6 +10,15 @@ state = {
 
     handleDelete  = (movie) => {
     const movies = this.state.movies.filter(m => m._id !== movie._id)
+        this.setState({movies})
+    }
+
+    handleLike  = (movie) => {
+   /* const movies = this.state.movies.map(m => !(m._id.liked));*/
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index] = {...movies}
+        movies[index].liked = !movies[index].liked;
         this.setState({movies})
     }
 
@@ -25,7 +36,8 @@ if(MoviesCount === 0)
                 <th>Genre</th>
                 <th>Stock</th>
                 <th>Rate</th>
-                <th></th>
+                <th/>
+                <th/>
             </tr>
             </thead>
             <tbody>
@@ -35,16 +47,18 @@ if(MoviesCount === 0)
                     <td>{movie.genre.name} </td>
                     <td>{movie.numberInStock}</td>
                     <td>{movie.dailyRentalRate} </td>
+                    <td> <Like liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}/> </td>
                     <td><button className="btn btn-danger btn-sm"
             onClick={() => this.handleDelete(movie)}>Delete</button></td>
                 </tr>
             })}
             </tbody>
         </table>
+       <Pagination/>
         </React.Fragment>
         );
     }
-
 }
 
 export default Movies;
