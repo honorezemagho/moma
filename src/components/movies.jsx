0a494c8e1a6cc29baf05860f3645aacf901e9508
honorezemagho <honorezemagho@gmail.com>
2020-06-13 21:23:47 +0100

    Moving movies table into a component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import {getMovies} from "../services/fakeMovieService";
 import {getGenres} from '../services/fakeGenreService';
-import Like from './common/like';
 import ListGroup from "./common/listGroup";
+import MoviesTable from "./moviesTable";
 import Pagination from "./common/pagination";
 import {paginate} from '../utils/paginate';
 
@@ -64,34 +64,11 @@ if(MoviesCount === 0)
                 </div>
                     <div className="col-md-9">
                 <p> Showing {filtered.length} movies in the database</p>
-        <table className="table">
-            <thead>
-            <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th/>
-                <th/>
-            </tr>
-            </thead>
-            <tbody>
-            {movies.map(movie => {
-                return <tr key={movie._id}>
-                    <td>{movie.title} </td>
-                    <td>{movie.genre.name} </td>
-                    <td>{movie.numberInStock}</td>
-                    <td>{movie.dailyRentalRate} </td>
-                    <td>
-                        <Like liked={movie.liked}
-                    onClick={() => this.handleLike(movie)}/>
-                    </td>
-                    <td><button className="btn btn-danger btn-sm"
-            onClick={() => this.handleDelete(movie)}>Delete</button></td>
-                </tr>
-            })}
-            </tbody>
-        </table>
+
+                        <MoviesTable movies={movies}
+                        onDelete = {this.handleDelete}
+                        onLike={this.handleLike}/>
+
                 <Pagination itemsCount={filtered.length}
                             pageSize={pageSize}
                             currentPage={currentPage}
