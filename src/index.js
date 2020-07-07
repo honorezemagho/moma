28b7300ee909c0368b86c6e521792156ca440706
honorezemagho <honorezemagho@gmail.com>
2020-07-07 22:59:03 +0100

    adding browser router for routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 import bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 let styles = bootstrap;
 
-ReactDOM.render(<App className={styles}/>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter>
+    <App className={styles}/>
+</BrowserRouter>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
