f2d776596dcc8d3b9e5e97707b895f59f96b890f
honorezemagho <honorezemagho@gmail.com>
2020-07-07 22:59:59 +0100

    adding some routes and redirects

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React , {Component} from 'react';
-import './App.css';
+import {Route, Redirect, Switch} from "react-router-dom";
 import Movies from "./components/movies";
+import MovieForm  from './components/movieForm';
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/not-Found";
+import NavBar from "./components/navBar";
+import './App.css';
 
 class App extends Component{
    render(){
   return (
-    <main className="container">
-    <Movies/>
-    </main>
+    <React.Fragment>
+      <NavBar/>
+        <main className="container">
+        <Switch>
+        <Route path="/movies/:id" component={MovieForm}/>
+        <Route path="/movies" component={Movies}/>
+        <Route path="/customers" component={Customers}/>
+        <Route path="/rentals" component={Rentals}/>
+        <Route path="/not-found" component={NotFound}/>
+         <Route exact path="/">
+           <Redirect to="/movies"/>
+         </Route>
+          <Route>
+            <Redirect to="/not-found"/>
+          </Route>
+        </Switch>
+        </main>
+    </React.Fragment>
   );
  }
 }
