34fbd99422e121d209e6f62385cf94c222ace9b1
honorezemagho <honorezemagho@gmail.com>
2020-07-10 18:05:23 +0000

    adding some basic validations

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -26,23 +26,41 @@ class LoginForm extends Component{
         e.preventDefault();
         //Call the server
         const errors = this.validate();
-        console.log(errors);
+        this.setState({errors:errors || {}});
+         if(errors) return;
         console.log('submitted');
     }
 
+
+    validateProperty = ({name, value}) => {
+        if(name === 'username'){
+            if(value.trim() === '') return "Username is required.";
+        }
+
+         if(name === 'password'){
+            if(value.trim() === '') return "Password is required.";
+        }
+    }
+
+
     handleChange = ({currentTarget: input})=>{
+        const errors = {...this.state.errors};
+        const errorMessage = this.validateProperty(input);
+        if(errorMessage) errors[input.name] = errorMessage;
+        else delete errors[input.name];
+
         const account = {...this.state.account};
         account[input.name] = input.value;
-        this.setState({account});
+        this.setState({account, errors});
     }
 
     render(){
-        const { account } = this.state;
+        const { account, errors } = this.state;
     return( <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-            <Input name="username" value={account.username} label="Username" onChange={this.handleChange}/>
-            <Input name="password" value={account.password} label="Password" onChange={this.handleChange}/>
+            <Input name="username" value={account.username} label="Username" onChange={this.handleChange} error={errors.username}/>
+            <Input name="password" value={account.password} label="Password" onChange={this.handleChange} error={errors.password}/>
         <button className="btn btn-primary">Login</button>
         </form>
     </div>)};
