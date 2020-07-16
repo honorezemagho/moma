9425232233b8d9104898dadeb1aa63d093d2c56d
honorezemagho <honorezemagho@gmail.com>
2020-07-16 14:44:46 +0000

    adding SearchBox component to movies

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -4,6 +4,7 @@ import {getGenres} from '../services/fakeGenreService';
 import ListGroup from "./common/listGroup";
 import MoviesTable from "./moviesTable";
 import Pagination from "./common/pagination";
+import SearchBox from "./common/searchbox";
 import {paginate} from '../utils/paginate';
 import _  from 'lodash';
 import { Link } from 'react-router-dom';
@@ -16,6 +17,8 @@ state = {
     selectedGenre: 0,
     currentPage: 1,
     pageSize: 4,
+    searchQuery: "",
+    selectedGenre: null,
     sortColumn : {path:'title', order: 'asc'}
 }
 
@@ -43,21 +46,31 @@ componentDidMount() {
     }
 
     handleGenreSelect = genre =>{
-        this.setState({selectedGenre: genre, currentPage: 1});
+        this.setState({selectedGenre: genre, searchQuery:"", currentPage: 1});
+    }
+
+    handleSearch = query => {
+        this.setState({searchQuery: query, selectedGenre:null, currentPage:1})
     }
 
     handleSort = sortColumn => {
         this.setState({sortColumn})
     }
 
+
     getPageData = () => {
-        const {pageSize, currentPage,movies: allMovies, selectedGenre,sortColumn} = this.state;
+        const {pageSize, currentPage,movies: allMovies, selectedGenre,sortColumn, searchQuery} = this.state;
 
-        const filtered = selectedGenre && selectedGenre._id
-            ? allMovies.filter(m => m.genre._id === selectedGenre._id)
-            : allMovies;
+        let filtered = allMovies;
+        if(searchQuery)
+            filtered = allMovies.filter(
+                m => m.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+            );
+        
+        else if (selectedGenre && selectedGenre._id)
+            filtered =  allMovies.filter(m => m.genre._id === selectedGenre._id);
 
-        const sorted = _.orderBy(filtered,[sortColumn.path],[sortColumn.order])
+        const sorted = _.orderBy(filtered,[sortColumn.path],[sortColumn.order]);
 
         const movies = paginate(sorted, currentPage, pageSize);
 
@@ -71,7 +84,7 @@ if(MoviesCount === 0)
     return <p> There are no movies in the database</p>;
 
         const {totalCount, data: movies} = this.getPageData();
-        const {pageSize, currentPage,sortColumn} = this.state;
+        const {pageSize, currentPage,sortColumn, searchQuery} = this.state;
 
         return( <div className="row">
 
@@ -84,6 +97,7 @@ if(MoviesCount === 0)
                         <Link to="/movies/new" className="btn btn-primary" style={{marginBottom: 20}}> New Movie </Link>
                 <p> Showing {totalCount} movies in the database</p>
 
+                        <SearchBox value={searchQuery} onChange={this.handleSearch}/>
                         <MoviesTable
                          movies={movies}
                          sortColumn={sortColumn}
